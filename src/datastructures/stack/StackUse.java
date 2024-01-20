7fda314dad6f78b83e22316d56be81508225cafe
Sai Kumar Reddy <saikumarreddy@Sais-MacBook-Air-5.local>
2024-01-20 12:36:01 +0530

    Added double capacity function by removing the stack full exception

--- src/datastructures/stack/StackUse.java
+++ src/datastructures/stack/StackUse.java
@@ -5,8 +5,12 @@ public class StackUse {
 	public static void main(String[] args) throws StackFullException, StackEmptyException {
 		// TODO Auto-generated method stub
 		
-		StackUsingArray stack=new StackUsingArray();
+		StackUsingArray stack=new StackUsingArray(2);
 		stack.push(30);
+		stack.push(90);
+		stack.push(80);
+		stack.push(40);
+		stack.push(60);
 		System.out.println(stack.size());
 		System.out.println(stack.isEmpty());
 		System.out.println(stack.top());
--- src/datastructures/stack/StackUsingArray.java
+++ src/datastructures/stack/StackUsingArray.java
@@ -2,28 +2,34 @@ package datastructures.stack;
 
 public class StackUsingArray {
 	
-	private int data[];
-	private int topIndex;
+	private int data[];    //Array we use to build for stack
+	private int topIndex;  //Index at the top of the stack
 	
+	//constructor
 	public StackUsingArray() {
 		data=new int[10];
 		topIndex=-1;
 	}
 	
+	//constructor with size
+	public StackUsingArray(int size) {
+		data=new int[size];
+		topIndex=-1;
+	}
 	
 	
-	public void push(int ele) throws StackFullException {
+	
+	public void push(int ele)  {
 		
 		//If stack is full
 		if(topIndex==data.length - 1) {
-			StackFullException e=new StackFullException();
-			throw e; // Throw exception
+			//StackFullException e=new StackFullException();
+			//throw e; // Throw exception
+			doubleCapacity();
+			System.out.println("double capacity function used");
+			
 			
 		}
-		
-		
-		
-		
 		
 		topIndex++;
 		data[topIndex]=ele ;
@@ -32,6 +38,16 @@ public class StackUsingArray {
 		
 		}
 	
+	private void doubleCapacity() {
+		// TODO Auto-generated method stub
+		int[] temp=data;
+		data=new int[temp.length*2];
+		for(int i=0;i<temp.length;i++) {
+			data[i]=temp[i];
+		}
+		
+	}
+
 	public int size() {
 		
 		return topIndex+1;
